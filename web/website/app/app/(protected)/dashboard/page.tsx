40b35275d44503c80bb0f8e4fd2160ba84401426
Martin-Hayot <m.hayot@students.ephec.be>
2024-02-08 21:11:09 +0100

    course page bug fix

--- web/website/app/app/(protected)/dashboard/page.tsx
+++ web/website/app/app/(protected)/dashboard/page.tsx
@@ -1,10 +1,29 @@
-import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardHeader,
+} from "@/components/ui/card";
 
+import { Button } from "@/components/ui/button";
 import { ScrollArea } from "@/components/ui/scroll-area";
-import { Courses } from "../_components/courses";
+import { db } from "@/lib/db";
+import { Course } from "@prisma/client";
 import { CourseDialog } from "../_components/new-course-dialog";
 
 const DashboardPage = async () => {
+    const courses = await db.course.findMany();
+
+    if (!courses) {
+        return (
+            <Card className="w-[600px]">
+                <CardHeader className="text-2xl font-semibold flex justify-center items-center flex-row">
+                    Dashboard
+                </CardHeader>
+                <CardContent>No courses at the moment</CardContent>
+            </Card>
+        );
+    }
     return (
         <Card className="w-[600px]">
             <CardHeader className="text-2xl font-semibold flex justify-center items-center flex-row">
@@ -13,7 +32,24 @@ const DashboardPage = async () => {
             <CardContent>
                 <CourseDialog />
                 <ScrollArea className="h-[400px]">
-                    <Courses />
+                    <div className="flex flex-col gap-y-6">
+                        {courses.map((course: Course) => (
+                            <Card
+                                key={course.id}
+                                className="flex flex-row justify-between items-center"
+                            >
+                                <CardHeader>
+                                    {course.title}
+                                    <CardDescription>
+                                        {course.description}
+                                    </CardDescription>
+                                </CardHeader>
+                                <CardContent className="mt-6">
+                                    <Button size="lg">Join</Button>
+                                </CardContent>
+                            </Card>
+                        ))}
+                    </div>
                 </ScrollArea>
             </CardContent>
         </Card>
